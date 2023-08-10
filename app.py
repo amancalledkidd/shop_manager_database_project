e3ee16dbfebeebf382aa6d844e60090a6328cc1a
Kumani K <kumanikidd@gmail.com>
2023-08-10 14:00:45 +0100

    project setup

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from lib.database_connection import DatabaseConnection
-from lib.artist_repository import ArtistRepository
+
 
 
 # Connect to the database
@@ -7,12 +7,5 @@ connection = DatabaseConnection()
 connection.connect()
 
 # Seed with some seed data
-connection.seed("seeds/music_library.sql")
+connection.seed("")
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
